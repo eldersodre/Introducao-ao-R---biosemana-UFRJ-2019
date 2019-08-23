928766a45b01ae128f72bb8bfc79cdc8b2b7755d
eldersodre <elder.limnologia@gmail.com>
2019-08-23 12:04:02 -0300

    Add files via upload

---
+++ Materiais para o curso/script.R
@@ -0,0 +1,10 @@
+###################################################################
+
+leia.me <- "Você conseguiu usar a função source() corretamente."
+leia.me2 <- "Parabéns! Continue assim!"
+
+
+##################################################
+
+print("Deu certo!!! Você conseguiu!!! Agora veja que dois objetos foram adicionados aos seus: leia.me e leia.me2. Abra-os.")
+
